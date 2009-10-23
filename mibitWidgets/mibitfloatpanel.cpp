36623c35ef551bc5b4359df948286e90f336f1dd
Miguel Chavez Gamboa <miguel@lemonpos.org>
2009-10-23 11:21:30 -0500

    Adding mibitWidgets

---
+++ mibitWidgets/mibitfloatpanel.cpp
@@ -0,0 +1,217 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Miguel Chavez Gamboa                            *
+ *   miguel@lemonpos.org                                                   *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Lesser General  Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Lesser General  Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General  Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#include "mibitfloatpanel.h"
+
+#include <QPixmap>
+#include <QString>
+#include <QHBoxLayout>
+#include <QTimeLine>
+#include <QTimer>
+#include <QMouseEvent>
+#include <QEvent>
+#include <QDebug>
+
+MibitFloatPanel::MibitFloatPanel(QWidget *parent, const QString &file, PanelPosition position)
+        : QSvgWidget( parent )
+{
+    //setMouseTracking(true);
+
+    if (file != 0) setSVG(file);
+
+    m_position = position;
+    m_parent = parent;
+    m_fileName = file;
+    canBeHidden = false; //at the begining is hidden.
+    setMinimumHeight(100);
+    setMinimumWidth(100);
+    setMaxHeight(100);
+    setMaxWidth(100);
+    setFixedHeight(0);
+    animRate = 500; //default animation speed (half a second rate).
+
+    hLayout    = new QHBoxLayout();
+    setLayout(hLayout);
+
+    //Postition it on its place
+    reposition();
+
+    timeLine  = new QTimeLine(animRate, this);
+    connect(timeLine, SIGNAL(frameChanged(int)), this, SLOT(animate(int)));
+    connect(timeLine,SIGNAL(finished()), this, SLOT(onAnimationFinished()));
+}
+
+void MibitFloatPanel::reposition()
+{
+    QRect windowGeom = m_parent->geometry();
+    int midPointX = (windowGeom.width()/2);
+    int midPointY = (windowGeom.height()/2);
+    int newX; int newY;
+    QRect dRect;
+    if ((midPointX-(maxWidth/2)) < 0) newX = 0; else newX = midPointX - (maxWidth/2);
+    if ((midPointY-(maxHeight/2)) < 0) newY = 0; else newY = midPointY - (maxHeight/2);
+
+    switch (m_position) {
+        case Top:
+            newY = 10-maxHeight;
+        break;
+        case Bottom:
+            newY = m_parent->height()+height()-10;
+        break;
+        case Left:
+            newX = 10-maxWidth;
+        break;
+        case Right:
+            newX = m_parent->width()-10;
+        break;
+    }
+
+    dRect.setX(newX);
+    dRect.setY(newY);
+    setFixedWidth(maxWidth); //width maybe is not yet defined.
+    setFixedHeight(maxHeight);
+    setGeometry(dRect);
+}
+
+void MibitFloatPanel::addWidget(QWidget * widget)
+{
+    hLayout->addWidget(widget, 0, Qt::AlignCenter);
+}
+
+void MibitFloatPanel::showPanel()
+{
+    if (timeLine->state() == QTimeLine::NotRunning && !canBeHidden) {
+        setGeometry(-1000,-1000,0,0);
+        show();
+        //update steps for animation, now that the panel is showing.
+        int maxStep; int minStep = 0;
+
+        switch (m_position) {
+            case Top :
+                minStep = -maxHeight+10;
+                maxStep =  -6;
+            break;
+            case Bottom:
+                maxStep = m_parent->geometry().height()-maxHeight;
+                minStep = m_parent->geometry().height()-10;
+            break;
+            case Left:
+                minStep = -maxWidth+10;
+                maxStep = -6;
+            break;
+            case Right:
+                minStep = m_parent->geometry().width()-10;
+                maxStep = m_parent->geometry().width()-maxWidth;
+            break;
+            default:
+            break;
+        }
+
+        timeLine->setFrameRange(minStep,maxStep);
+        //make it grow
+        timeLine->setDirection(QTimeLine::Forward);
+        timeLine->start();
+
+    } 
+}
+
+void MibitFloatPanel::animate(const int &step)
+{
+    //get some sizes...
+    QRect windowGeom = m_parent->geometry();
+    int midPointX = (windowGeom.width()/2);
+    int midPointY = (windowGeom.height()/2);
+    int newX;  int newY;
+    QRect dRect;
+    if ((midPointX-(maxWidth/2)) < 0) newX = 0; else newX = midPointX - (maxWidth/2);
+    if ((midPointY-(maxHeight/2)) < 0) newY = 0; else newY = midPointY - (maxHeight/2);
+
+
+    switch (m_position) {
+        case Bottom:
+        case Top:
+            dRect.setX(newX);
+            dRect.setY(step);
+        break;
+        case Left:
+        case Right:
+            dRect.setY(newY);
+            dRect.setX(step);
+        break;
+        default:
+        break;
+    }
+
+    setFixedHeight(maxHeight);
+    setFixedWidth(maxWidth);
+    setGeometry(dRect);
+}
+
+void MibitFloatPanel::hideOnUserRequest()
+{
+    hideDialog();
+}
+
+void MibitFloatPanel::hideDialog()
+{
+    if ( canBeHidden ) {
+        timeLine->setDirection(QTimeLine::Backward); //reverse!
+        timeLine->start();
+    }
+}
+
+void MibitFloatPanel::onAnimationFinished()
+{
+    if (timeLine->direction() == QTimeLine::Forward) {
+        canBeHidden = true;
+    } else canBeHidden = false;
+}
+
+void MibitFloatPanel::setPosition(const PanelPosition pos)
+{
+    // only changes the position when the notification is not showing..
+    if (timeLine->state() == QTimeLine::NotRunning && !canBeHidden) {
+        m_position = pos;
+        //recalculate its rect and show it there...
+        reposition();
+    }
+}
+
+
+//NOTE: The svg file is not rendered correctly. It does not render the blur, i.e. the shadows
+void MibitFloatPanel::setSVG(const QString &file)
+{
+    load(file);
+}
+
+
+void MibitFloatPanel::enterEvent ( QEvent * )
+{
+    QTimer::singleShot(100,this,SLOT(showPanel()));
+}
+
+void MibitFloatPanel::leaveEvent( QEvent * )
+{
+    QTimer::singleShot(100,this,SLOT(hideOnUserRequest()));
+}
+
+MibitFloatPanel::~MibitFloatPanel() {}
+
+
---
+++ mibitWidgets/mibitfloatpanel.h
@@ -0,0 +1,84 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Miguel Chavez Gamboa                            *
+ *   miguel@lemonpos.org                                                   *
+ *                                                                         *
+ *   This library is free software; you can redistribute it and/or         *
+ *   modify it under the terms of the GNU Lesser General  Public           *
+ *   License as published by the Free Software Foundation; either          *
+ *   version 2 of the License, or (at your option) any later version.      *
+ *                                                                         *
+ *   This library is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU Lesser General  Public License for more details.                  *
+ *                                                                         *
+ *   You should have received a copy of the GNU Lesser General  Public     *
+ *   License along with this program; if not, write to the                 *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef MIBITFLOATPANEL_H
+#define MIBITFLOATPANEL_H
+
+#include <QSvgWidget>
+class QTimeLine;
+class QString;
+class QHBoxLayout;
+class QLabel;
+class QPixmap;
+
+/**
+  * This class is used to display an animated floating panel
+  * on Top, Bottom, Left or Right edges of its parent.
+  * Svg themed and borderless.
+  *
+  * The panel content is added with the addWidget() method,
+  * The simple way is to create a QWidget with all its content
+  * with its layout -in designer or by code-.
+  * It will be reparented automatically when added to the panel.
+  * Dont forget to set the panel size according to your needs.
+  *
+  */
+
+
+    enum PanelPosition  {Top = 1, Bottom = 2, Left = 3, Right = 4 };
+    enum PanelConstants {pMinH= 100, pMinW = 100 };
+
+
+class MibitFloatPanel : public QSvgWidget
+{
+Q_OBJECT
+public:
+    MibitFloatPanel(QWidget *parent = 0, const QString &file = 0, PanelPosition position = Top);
+    ~MibitFloatPanel();
+    void addWidget(QWidget * widget);
+    void hidePanel();
+    void setPosition(const PanelPosition pos);
+    void setSVG(const QString &file);
+    void setMaxHeight(const int &m)   { setMaximumHeight(m); maxHeight = m; }
+    void setMaxWidth(const int &m)   { setMaximumWidth(m); maxWidth = m; }
+    void setSize( const int &w, const int &h ) { setMaxHeight(h); setMaxWidth(w); }
+private:
+    QTimeLine *timeLine;
+    QHBoxLayout *hLayout;
+    QWidget *m_parent;
+    QString m_fileName;
+    bool canBeHidden;
+    int maxHeight;
+    int maxWidth;
+    int animRate;
+    PanelPosition m_position;
+    void reposition();
+private slots:
+    void animate(const int &step);
+    void hideDialog();
+    void hideOnUserRequest();
+    void onAnimationFinished();
+    void showPanel();
+protected:
+    void enterEvent ( QEvent * event );
+    void leaveEvent ( QEvent * event );
+};
+
+#endif // MIBITFLOATPANEL_H
