19e96ab250c9123941cf34a92937a4130ef790f1
Miguel Chavez Gamboa <miguel@lemonpos.org>
2010-02-02 07:36:26 -0600

    Updating/Merging enums.h

--- src/enums.h
+++ src/enums.h
@@ -25,11 +25,13 @@
 enum TransactionType  {tSell=1, tBuy=2, tChange=3, tReturn=4, tPayment=5, tOutOther=6};
 enum PaymentType      {pUnknown=99, pCash=1, pCard=2};
 enum cashFlowType     {ctCashOut=1, ctCashOutMoneyReturnOnCancel=2, ctCashOutMoneyReturnOnProductReturn=3, ctCashIn=4};
-enum TransactionState {tNotCompleted=1, tCompleted=2, tCancelled=3};
+enum TransactionState {tNotCompleted=1, tCompleted=2, tCancelled=3, tPOPending=4, tPOCompleted=5, tPOIncomplete=6};
 enum SellUnits        {uPiece=1, uWeightKg=2, uLengthMts=3, uVolumeLitre=4, uVolumeCubicMts=5}; //not needed anymore stored on database
 enum                  {colCode=0, colDesc=1, colPrice=2, colQty=3, colUnits=4, colDisc=5, colDue=6}; //column names and numbers..
 enum                  {pageMain=0, pageSearch=1, pageReprintTicket=2, pageRerturnProducts=3, pageAdds=4};
 
 enum userRoles        {roleBasic=1, roleAdmin=2, roleSupervisor=3 };
+
+enum soStatus         {stPending=0, stInProgress=1, stReady=2, stDelivered=3, stCancelled=4};
 
 #endif
