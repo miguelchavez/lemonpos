112c95ebffb8d64bbea85a19e2618a24c46d4842
Miguel Chavez Gamboa <miguel@lemonpos.org>
2012-03-28 19:44:30 -0600

    Updating version, adding date

--- squeeze/src/main.cpp
+++ squeeze/src/main.cpp
@@ -29,7 +29,7 @@
 static const char description[] =
     I18N_NOOP("Squeeze your lemon!");
 
-static const char version[] = "0.9.5.0";
+static const char version[] = "0.9.5.0 | 25/03/2012";
 
 int main(int argc, char **argv)
 {
